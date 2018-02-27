eacc943d04feaa6d338699b6f265da002146d1e9
Jeremy Kuhnash <kuhnash@gmail.com>
2018-02-26 23:24:32 -0500

    adding example configurations to deploy the container w/o Catalog (using the marathon.json) and an example batch snippet inside a Jenkins freestyle project to create a test service for use by the Marathon plugin.

---
+++ jenkins_out_to_marathon-json.sh
@@ -0,0 +1,23 @@
+rm marathon.json
+tee marathon.json &> /dev/null << EOF
+{
+  "id": "/test",
+  "instances": 1,
+  "portDefinitions": [],
+  "container": {
+    "type": "MESOS",
+    "volumes": []
+  },
+  "cpus": 0.1,
+  "mem": 128,
+  "requirePorts": false,
+  "networks": [],
+  "healthChecks": [],
+  "fetch": [],
+  "constraints": [],
+  "cmd": "echo 'hello' && sleep infinity"
+}
+
+EOF
+
+echo marathon.json
